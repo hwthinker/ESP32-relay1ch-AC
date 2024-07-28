62fde3f1a3cc35d1252914d69953959c92dda836
hwthinker <hwthinker@gmail.com>
2024-07-28 16:00:16 +0700

    update picture

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,12 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(RLY1, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
+  delay(100);                      // wait for a second
   digitalWrite(RLY1, LOW);   // turn the LED off by making the voltage LOW
-  delay(100);    
+  delay(1000);    
   
   digitalWrite(LED, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
   digitalWrite(LED, LOW);   // turn the LED off by making the voltage LOW
-  delay(100);                   // wait for a second
+  delay(1000);                   // wait for a second
 }
